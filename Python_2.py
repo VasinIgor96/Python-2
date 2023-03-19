970c0b6fe4b76b40304f53c49ca2ab543883209d
student <student@PC-G-04>
2023-03-19 11:26:34 +0200

    Ex_2

--- Python_2.py
+++ Python_2.py
@@ -1,1 +1,17 @@
 
+x = int(input('Введіть число:'))
+
+x_1 = int(x/1000)
+
+x_2 = int(x/100)%10
+
+x_3 = int(x/10)%10
+
+x_4 =int(x%10)
+
+print(x_1)
+print(x_2)
+print(x_3)
+print(x_4)
+
+print(x_1*x_2*x_3*x_4)
