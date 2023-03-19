758c5f605243f537ac36f2149dbe700b0c7139a0
Vasin Igor <vasin.igor0796@gmail.net>
2023-03-19 23:09:23 +0200

    Ex_5 Task done

--- Python_2.py
+++ Python_2.py
@@ -1,1 +1,16 @@
+num = int(input('Введіть число яке бажаєте перевернути:'))
+reverse_num = 0
 
+reverse_num = reverse_num * 10 + num % 10
+num = num // 10
+
+reverse_num = reverse_num * 10 + num % 10
+num = num // 10
+
+reverse_num = reverse_num * 10 + num % 10
+num = num // 10
+
+reverse_num = reverse_num * 10 + num % 10
+num = num // 10
+
+print(reverse_num)
