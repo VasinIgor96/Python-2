e785d1933971e281fcde4a5f2a178fa7dfaa47cb
student <student@PC-G-04>
2023-03-19 11:32:34 +0200

    Ex_3 task done

--- Python_2.py
+++ Python_2.py
@@ -1,1 +1,10 @@
 
+meter = float(input('Кількість метрів: '))
+
+print(f'{meter} метрів = {meter*10} дециметрів')
+
+print(f'{meter} метрів = {meter*100} сантиметрів')  
+
+print(f'{meter} метрів = {meter*1000} миліметрів')
+
+print(f'{meter} метрів = {meter/1609} миль')
